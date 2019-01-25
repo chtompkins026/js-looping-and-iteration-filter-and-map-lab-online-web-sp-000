3eff991092fa4984b2d4b0ce7c861691811cc33e
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-25 15:46:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function exactMatch(drivers, obj) {
   });
 }
 
-function exactMatch(drivers, obj) {
+function exactMatchToList(drivers, obj) {
   return drivers.filter(function (dman) {
     for(const key in obj) {
       return dman[key] === obj[key]
